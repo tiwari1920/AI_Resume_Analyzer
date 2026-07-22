7c8283fcc91c1b1de9160c4d735528c6000f8f0c
Satyam Tiwari <iamsatyampandit@gmail.com>
2026-07-22 23:29:12 +0530

    Extraction and Display of resume information

--- analyzer/views.py
+++ analyzer/views.py
@@ -12,9 +12,10 @@ def home(request):
          resume_text = extract_resume_text(file_path)
 # Step 2: Extract information from the resume text
          resume_information = extract_resume_information(resume_text)
-         return render(request, 'home.html', {
+      return render(request, 'home.html', 
+         {
              'message': 'Resume analyzed successfully!',
              'resume_text': resume_text,
              'resume_information': resume_information
          })
-         return render(request, 'home.html')
+   return render(request, 'home.html')
